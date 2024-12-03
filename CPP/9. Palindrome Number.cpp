8009c8bbafe878923cc1d5178cb51981c3466382
wk1104 <90096602+1103r74t8g@users.noreply.github.com>
2024-12-04 00:23:22 +0800

    Create 9. Palindrome Number.cpp

---
+++ CPP/9. Palindrome Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(long int x) {
+        if(x<0){
+            return false;
+        }
+        else{
+            string s1 = to_string(x);
+            string s2 = s1;              
+            reverse(s2.begin(), s2.end()); 
+            return s1 == s2;              
+        }
+    }
+};
