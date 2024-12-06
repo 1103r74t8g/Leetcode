aa19257773f6e62e923509bd2581ea43ec0f3b1c
wk1104 <90096602+1103r74t8g@users.noreply.github.com>
2024-12-06 23:35:15 +0800

    Array, Prefix Sum

---
+++ CPP/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxHeight = 0;
+        int current = 0;
+        for(int i = 0; i < gain.size(); i++){
+            current += gain[i];
+            maxHeight = max(maxHeight, current);
+        }
+        return maxHeight;
+    }
+};
